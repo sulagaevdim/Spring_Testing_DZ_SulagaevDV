bd2c17a43268593c3d48077c0871344258cd8cd2
Дмитрий <sulagaevdim@mail.ru>
2024-10-24 21:25:28 +0400

    init project

---
+++ src/main/java/ru/sulagaev/spring_testing_dz_sulagaevdv/SpringTestingDzSulagaevDvApplication.java
@@ -0,0 +1,13 @@
+package ru.sulagaev.spring_testing_dz_sulagaevdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingDzSulagaevDvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestingDzSulagaevDvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sulagaev/spring_testing_dz_sulagaevdv/SpringTestingDzSulagaevDvApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sulagaev.spring_testing_dz_sulagaevdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingDzSulagaevDvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
